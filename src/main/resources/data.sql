95da925bd18b799bc74df881a7bdf8638b1ea495
Florent Ros <ros-florent@hotmail.fr>
2021-12-20 15:41:14 +0100

    Projet terminé avec data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,9 +4,8 @@ INSERT INTO auteur VALUES (nextval('auteur_seq'), 'Durand' , 'Yves');
 INSERT INTO ouvrage(id, nombre_exemplaire, titre, auteur_id, maison_edition) VALUES (nextval('ouvrage_seq'), 2, 'Fables de la Fontaine' , 1 , 'Gallimard');
 INSERT INTO ouvrage (id, nombre_exemplaire, titre, auteur_id, maison_edition) VALUES (nextval('ouvrage_seq'), 1, 'Le monde' , 2 , 'Flammarion');
 
-INSERT INTO utilisateur(id, nom, is_admin, email, mot_de_passe) VALUES (nextval('utilisateur_seq'), 'Florent' , false, 'ros-florent@hotmail.fr' , '$2a$10$5Fz84nOqSte969IBf5iDU.FE7DqcB50XascRxDtYSdWVD//Mrida6');
-INSERT INTO utilisateur(id, nom, is_admin, email, mot_de_passe) VALUES (nextval('utilisateur_seq'), 'Serge' , false, 'ros-florent@hotmail.fr' , '$2a$10$VdWZvYM1mkoGBvZIvZde6.L4q3yf.IQJ4e2aR834mZbl6ofglRhrS');
-INSERT INTO utilisateur(id, nom, is_admin, email, mot_de_passe) VALUES (nextval('utilisateur_seq'), 'admin' , true, 'ros-florent@hotmail.fr' , 'admin13');
+INSERT INTO utilisateur(id, nom, is_admin, email, mot_de_passe) VALUES (nextval('utilisateur_seq'), 'Florent' , false, 'ros-florent@hotmail.fr' , '$2a$10$vRhObTPIOt/P7tjro/ft5ORDNvEvzu.5xBL9AVVtlcuieu/.JqYLK');
+INSERT INTO utilisateur(id, nom, is_admin, email, mot_de_passe) VALUES (nextval('utilisateur_seq'), 'Serge' , false, 'ros-florent@hotmail.fr' , '$2a$10$vRhObTPIOt/P7tjro/ft5ORDNvEvzu.5xBL9AVVtlcuieu/.JqYLK');
 
 INSERT INTO emprunt VALUES (nextval('emprunt_seq'), '2021-04-11' , '2021-05-11' , null, false,'2021-04-11', 1, 1);
 INSERT INTO emprunt VALUES (nextval('emprunt_seq'), '2021-02-11' , '2021-03-11' , null, false,'2021-04-11', 1, 2);
